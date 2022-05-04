ba395edcad5238d463b5a686c1b75777efe99754
SamIsTheFBI <mrqube001@gmail.com>
2022-05-04 08:58:50 +0530

    Changed color scheme

--- colors-wal-dmenu.h
+++ colors-wal-dmenu.h
@@ -1,6 +1,6 @@
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#c4c0c1", "#091019" },
-	[SchemeSel] = { "#c4c0c1", "#226CA2" },
-	[SchemeOut] = { "#c4c0c1", "#2C91D8" },
+	[SchemeNorm] = { "#ccc0ae", "#15140b" },
+	[SchemeSel] = { "#ccc0ae", "#D4102A" },
+	[SchemeOut] = { "#ccc0ae", "#618D57" },
 };
