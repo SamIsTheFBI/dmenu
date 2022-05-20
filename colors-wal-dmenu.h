302197ed02991dd42e0f242eadec780a94c68c37
SamIsTheFBI <mrqube001@gmail.com>
2022-05-20 22:23:42 +0530

    changed to Cafe theme
    This is supposed to be a warmer theme actually. Whatever it is right now, I'm just calling it Cafe theme. Probably gonna regret

--- colors-wal-dmenu.h
+++ colors-wal-dmenu.h
@@ -1,6 +1,6 @@
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#ccc0ae", "#15140b" },
-	[SchemeSel] = { "#ccc0ae", "#D4102A" },
-	[SchemeOut] = { "#ccc0ae", "#618D57" },
+	[SchemeNorm] = { "#EADAB1", "#322119" },
+	[SchemeSel] = { "#EADAB1", "#C3763A" },
+	[SchemeOut] = { "#EADAB1", "#5E8485" },
 };
