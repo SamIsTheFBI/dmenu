2cfada77e2b4f8b2c3ad191f9b0ac5eaa897344b
SamIsTheFBI <mrqube001@gmail.com>
2022-07-08 19:04:31 +0530

    added mouse support

--- config.h
+++ config.h
@@ -8,9 +8,12 @@ static const char *fonts[] = {
 	"emoji:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-
-#include "dmenu-colors.h"
-
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 
--- dmenu-colors.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#EADAB1", "#322119" },
-	[SchemeSel] = { "#EADAB1", "#C3763A" },
-	[SchemeOut] = { "#EADAB1", "#5E8485" },
-};
